d119d3b78672d7ddcff68beca392691d5122133a
Matt Beachey <mattbeachey@gmail.com>
2020-01-25 12:10:07 -0600

    using state to let user determine how many employees render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EmployeeCard from './components/EmployeeCard'
 import './App.css';
 
 function App() {
+
+  const [reduceBy, setReduceBy] = useState(0)
+
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+  for (let i = 0; i < reduceBy; i++) {
+    array.pop()
+  }
+
   return (
-    <div className="App">
-      <EmployeeCard />
+    <div>
+      <button onClick={() => setReduceBy(reduceBy + 1)}>
+        Increment
+    </button>
+      <button onClick={() => setReduceBy(reduceBy - 1)}>
+        Decremet
+    </button>
+      <div className="App">
+        {array.map((item) => (
+          <EmployeeCard />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -7,11 +7,15 @@ function EmployeeCard() {
 
     const { employee, getEmployee } = useGetEmployee()
     console.log(employee.name)
+    console.log(employee)
 
     return (
         <div>
-            <h1>{employee.gender}</h1>
-           
+            <h1>{employee.name && employee.name.first} {employee.name && employee.name.last}</h1>
+            <h2>{employee.location && employee.location.city}, {employee.location && employee.location.country}</h2>
+            <h3>{employee.email} </h3>
+            <h3>{employee.phone} </h3>
+            <img src={employee.picture && employee.picture.large}/>
         </div>
 
 
