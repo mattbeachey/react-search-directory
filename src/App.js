95c674c0c19cbbbd7aacbb5195d73385836b59b2
Matt Beachey <mattbeachey@gmail.com>
2020-01-28 21:18:56 -0600

    dynamic search, revert to original

--- src/App.js
+++ src/App.js
@@ -3,60 +3,67 @@ import axios from 'axios'
 import EmployeeCard from './components/EmployeeCard'
 import './App.css';
 
+let finishedDisplays = 30;
+const employeeArray = []
+const revertedEmployeeArray = []
+
 function App() {
   const [finalEmployeeArray, setFinalEmployeeArray] = useState([])
 
   //an array of random users is created from randomuser.me/api
-  const employeeArray = []
-  const numberDisplayed = 10;
-  for (let i = 0; i < numberDisplayed; i++) {
-    const randomNum = Math.floor(Math.random() * 4);
-    let department;
-    switch(randomNum) {
-      case 0:
-        department = "Development";
-        break;
-      case 1:
-        department = "Marketing";
-        break;
-      case 2:
-        department = "Sales";
-        break;
-      case 3:
-        department = "Paper Pushing";
-        break;
-      default:
+  const numberDisplayed = 30;
+  if (finishedDisplays > 0) {
+    for (let i = 0; i < numberDisplayed; i++) {
+      const randomNum = Math.floor(Math.random() * 4);
+      let department;
+      switch (randomNum) {
+        case 0:
+          department = "Development";
+          break;
+        case 1:
+          department = "Marketing";
+          break;
+        case 2:
+          department = "Sales";
+          break;
+        case 3:
+          department = "Paper Pushing";
+          break;
+        default:
+      }
+      axios.get('https://randomuser.me/api/?nat=us')
+        .then(function (res) {
+          let employee = res.data.results[0]
+          const employeeObject = {
+            first: employee.name.first,
+            last: employee.name.last,
+            gender: employee.gender,
+            department: department,
+            city: employee.location.city,
+            country: employee.location.country,
+            email: employee.email,
+            phone: employee.phone,
+            image: employee.picture.large
+          }
+          revertedEmployeeArray.push(employeeObject)
+          employeeArray.push(employeeObject)
+          finishedDisplays--
+          if (finishedDisplays == 0) {
+            setFinalEmployeeArray(employeeArray)
+          }
+        });
     }
-    console.log(department, randomNum)
-    axios.get('https://randomuser.me/api/?nat=us')
-      .then(function (res) {
-        let employee = res.data.results[0]
-        const employeeObject = {
-          first: employee.name.first,
-          last: employee.name.last,
-          gender: employee.gender,
-          department: department,
-          city: employee.location.city,
-          country: employee.location.country,
-          email: employee.email,
-          phone: employee.phone,
-          image: employee.picture.large
-        }
-        employeeArray.push(employeeObject)
-      });
   }
 
-  function filterEmployees(i, category){
-    switch(category) {
+  function filterEmployees(i, category) {
+    switch (category) {
       case "byDepartment":
-        const array = finalEmployeeArray.filter(function(emp){
-          console.log(i)
+        const array = finalEmployeeArray.filter(function (emp) {
           return emp.department === i
         })
         return array
       case "byGender":
-        const array2 = finalEmployeeArray.filter(function(emp){
-          console.log(i)
+        const array2 = finalEmployeeArray.filter(function (emp) {
           return emp.gender === i
         })
         return array2
@@ -65,40 +72,54 @@ function App() {
 
   }
 
-  function sortArray(sortParam){
+  function sortArray(sortParam) {
     employeeArray.sort((a, b) => {
       if (a.last > b.last) {
-        return 1 
+        return 1
       } else {
         return -1
       }
     })
-    console.log(employeeArray)
     return employeeArray
+  }
+
+  function searchFilter(value){
+    const filteredEmpArray = employeeArray.slice().filter(function(employee){
+        const fullName = (employee.first + " " + employee.last ).toLowerCase();
+        return fullName.indexOf(value.toLowerCase()) > -1
+    })
+    setFinalEmployeeArray(filteredEmpArray)
   }
 
   return (
     <div>
-    <button onClick={() => setFinalEmployeeArray(employeeArray)}>
+      <button onClick={() => setFinalEmployeeArray(employeeArray)}>
         Get Employees
     </button>
-    <button onClick={() => setFinalEmployeeArray(sortArray())}>
+      <button onClick={() => setFinalEmployeeArray(sortArray())}>
         Sort By Last Name
     </button>
+      <button onClick={() => setFinalEmployeeArray(revertedEmployeeArray)}>
+        Show All Employees
+    </button>
+    <input onChange={event => {
+      const value = event.target.value
+      searchFilter(value);
+      }}/>
       <div className="cardBox">
         {finalEmployeeArray.map((employee) => (
           <EmployeeCard
-          first={employee.first}
-          last={employee.last}
-          gender={employee.gender}
-          department={employee.department}
-          city={employee.city}
-          country={employee.country}
-          email={employee.email}
-          phone={employee.phone}
-          image={employee.image}
-          handleClick={() => setFinalEmployeeArray(filterEmployees(employee.department, "byDepartment"))}
-          handleClick2={() => setFinalEmployeeArray(filterEmployees(employee.gender, "byGender"))}
+            first={employee.first}
+            last={employee.last}
+            gender={employee.gender}
+            department={employee.department}
+            city={employee.city}
+            country={employee.country}
+            email={employee.email}
+            phone={employee.phone}
+            image={employee.image}
+            handleClick={() => setFinalEmployeeArray(filterEmployees(employee.department, "byDepartment"))}
+            handleClick2={() => setFinalEmployeeArray(filterEmployees(employee.gender, "byGender"))}
           />
         ))}
       </div>
--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -10,12 +10,8 @@ function EmployeeCard({ first, last, gender, department, city, country, email, p
     return (
         <div className="card">
             <h1>{first} {last}</h1>
-            <h1 onClick={handleClick}>{department}</h1>
-            <select>
-                <option onChange={handleClick}>Sort alphabetacally by department</option>
-                <option >See only this department</option>
-            </select>
-            <h2 onClick={handleClick2}>{gender}</h2>
+            <h1 className="clickable" onClick={handleClick}>{department}</h1>
+            <h2 className="clickable" onClick={handleClick2}>{gender}</h2>
             <h2>{city}, {country}</h2>
             <h3>{email} </h3>
             <h3>{phone} </h3>
